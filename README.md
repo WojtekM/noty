93ad262420107bafc4e37676a1dc74540962e532
WojtekM <wojciech.mardyla@gmail.com>
2012-09-01 11:52:40 +0200

    Initial place-holder commit, nothing to see here, move along :)

---
+++ README.md
@@ -0,0 +1,8 @@
+### Welcome to
+
+# Noty(note)
+
+
+**Noty** is supposed to be small "just another lousy Web-notes" application, but with option to share boring notes publicly and to keep some more interesting, **noughty** *notes* just for your eyes :).
+
+Real work is coming soon (I hope :)) but for sure not before 20th of September, so move along, nothing to see here (yet:)).
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Noty::Application.config.secret_token = '269cb9fdcd6018b15807adba2e72142e53637dd232f63413e3373ebaf17de32500ec10fd0d31203e9b361dadf80f0aaa4bbb2beb40ec9e93374ae86796d59ee8'
